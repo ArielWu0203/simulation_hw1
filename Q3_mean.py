a7656dc93a7eee821db5613f1ca3820eb0d2c638
ArielWu0203 <lucky020372@gmail.com>
2019-10-02 20:53:35 +0800

    Q3 done

---
+++ Q3_mean.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import collections
+import matplotlib.pyplot as plt
+import numpy as np
+
+# TODO : get the count table
+def read(name):
+    df = pd.read_csv(name)
+    shape = df.shape
+    # TODO : get raws of WAP1~520
+    raws = df.iloc[0:shape[0],0:520]
+
+    # TODO : calculate mean
+    table = raws.mean(axis = 0)
+
+    return table
+
+
+# TODO : plot
+def plot_table(table):
+    x = list(range(1,521))
+    y = table.values
+
+    fig = plt.figure()
+    plt.xlabel('WAP number')
+    plt.ylabel('mean value')
+    plt.title('Distribution of the mean of each WAP')
+    plt.plot(x,y)
+    plt.xlim((0,521))
+    plt.gcf().autofmt_xdate()
+    fig.savefig('Q3_mean.png')
+
+if __name__ == '__main__':
+    table = read('TrainingData_new.csv')
+    plot_table(table)
